c6e0ae21e940b3e52c3e64da51ea22c0d09e7e16
DaniloTojeiro <danilotojeiro@live.com>
2024-03-05 20:09:13 -0300

    JavaScript e o NodeJs

---
+++ console.js
@@ -0,0 +1,9 @@
+//log registro no nosso console
+
+const minhaVar = true;
+const meuVar = "Palmeiras";
+
+console.log(245)
+console.log("eu sou um texto")
+console.log(minhaVar)
+console.log(meuVar)
---
+++ script.js
@@ -0,0 +1,7 @@
+//tipagem dinâmica
+//untyped não precisa declarar o TIPO da variavel
+
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true; 
+
